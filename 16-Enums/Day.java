c3826c07c31440ba1ff2e0532628e34ed67376b3
Amit Khatri <aamita96@gmail.com>
2025-10-08 16:48:13 +0530

    Few more explainations about the Enum added.

--- 16-Enums/Day.java
+++ 16-Enums/Day.java
@@ -1,9 +1,64 @@
 public enum Day {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAYY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    SUNDAY("Sunday"),
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday"),
+    SATURDAY("Saturday");
+
+    // We can define custom constructors inside the Enum.
+    // Constructors must be declared private or default access. This prevents creating instances outside the predefined list.
+    private Day(String capitalcase) {
+        // System.out.println("constructor called!");
+        this.capitalcase = capitalcase;
+    }
+
+    // We can define custom fields inside the Enum
+    private String capitalcase;
+
+    public String getCapitalCase() {
+        return capitalcase;
+    }
+
+    // We can define a method inside the Enum
+    public void display() {
+        System.out.println("Today is " + this.name());
+    }
+}   
+
+/**
+ *  At compile time, the Enum structure is converted into a final class.
+ *  Below is an example of how the enum will be converted which is actually just a sample.
+ * 
+    public final class Day extends java.lang.Enum<Day> {
+    public static final Day SUNDAY = new Day("SUNDAY", 0);
+    public static final Day MONDAY = new Day("MONDAY", 1);
+    public static final Day TUESDAY = new Day("TUESDAY", 2);
+    public static final Day WEDNESDAY = new Day("WEDNESDAY", 3);
+    public static final Day THURSDAY = new Day("THURSDAY", 4);
+    public static final Day FRIDAY = new Day("FRIDAY", 5);
+    public static final Day SATURDAY = new Day("SATURDAY", 6);
+
+    private static final Day[] VALUES = {
+        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+    };
+
+    private Day(String name, int ordinal) {
+        super(name, ordinal);
+    }
+
+    public static Day[] values() {
+        return VALUES.clone();
+    }
+
+    public static Day valueOf(String name) {
+        for (Day day : VALUES) {
+            if (day.name().equals(name)) {
+                return day;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant " + name);
+    }
 }
+ */
--- 16-Enums/Main.java
+++ 16-Enums/Main.java
@@ -1,7 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Day.MONDAY);
-        System.out.println(Day.TUESDAY);
-        System.out.println(Day.SUNDAY);
+        Day day = Day.MONDAY; // All the constants that you have written in enum is a constant of type Day.
+
+        System.out.println(day);
+        day.display();
+        System.out.println(day.getCapitalCase());
+
+        // int ordinal = monday.ordinal(); // ordinal fetches index of that particular day.
+        // System.out.println(ordinal);
+        // System.out.println("Day name from name() : " + monday.name()); // name() method can be used to perform some string operations.
+
+        // System.out.println(Day.TUESDAY);
+        // System.out.println(Day.SUNDAY);
+        
+        // Day weekday = Day.valueOf("WEDNESDAY"); // Converts a matching input String into the corresponding Enum instance.
+        // System.out.println(weekday);
+        // weekday.display();
+        
+
+        // Enums with Switch statements
+        // Traditional syntax of switch
+        switch(day) {
+            case MONDAY: 
+                System.out.println("MONDAY from Switch Statement");
+                break; // Required
+            case TUESDAY:
+                System.out.println("TUESDAY from Switch Statement");
+                break; // Required
+            default: 
+                System.out.println("Weekend arrived!");
+        }
     }
 }
